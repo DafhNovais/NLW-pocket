6b6621f6ecf08a090c002c3e6382d310f91356e3
Dafhne Novais <dafhnenovais@gmail.com>
2024-09-14 21:32:37 -0300

    Fundamentos 002

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-// Hello world
-const mensagem = "Hello me!"
-
-{
-    const mensagem = "Hello world!"
-    console.log(mensagem)
+// Arrays, objetos
+let meta = {
+    value: 'Ler um livro por mês',
+    addres: 2,
+    checked: true,
+    log: (info) => {
+        console.log(info)
+    }
 }
 
-console.log(mensagem)
+meta.value = "Não é mais ler um livro"
+meta.log(meta.value)
+
+// function // arrow function
+// const criarMeta = () => {}
+// function criarMeta = () {}
+
+
+// Hello world
+// const mensagem = "Hello me!"
+
+// {
+//     const mensagem = "Hello world!"
+//     console.log(mensagem)
+// }
+
+// console.log(mensagem)
