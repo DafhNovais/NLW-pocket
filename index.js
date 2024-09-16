3b95a79c17e0e4f57e71caf21feae828a2a08c91
Dafhne Novais <dafhnenovais@gmail.com>
2024-09-15 23:21:15 -0300

    Metas realizadas

--- index.js
+++ index.js
@@ -49,6 +49,23 @@ const listarMetas = async () => {
     console.log("Meta(s) marcadas como concluída(s)")
 }
 
+const metasRealizadas = async () => {
+    const realizadas = metas.filter((meta) => {
+        return meta.checked
+    })
+
+    if(realizadas.length == 0) {
+        console.log("Não existem metas realizadas! :(")
+        return
+    }
+
+    await select({
+        message: "Metas realizadas",
+        choices: [...realizadas]
+    })
+}
+
+
 const start = async () => {
 
     while(true) {
@@ -64,6 +81,10 @@ const start = async () => {
                     value: "listar"
                 },
                 {
+                    name: "Metas realizadas",
+                    value: "realizadas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -75,10 +96,15 @@ const start = async () => {
                 await cadastrarMeta()
                 console.log(metas)
                 break
+
             case "listar":
                 await listarMetas()
-                console.log(metas)
                 break
+
+            case "realizadas":
+                await metasRealizadas()
+                break
+
             case "sair":
                 console.log("Até a próxima!")
                 return
